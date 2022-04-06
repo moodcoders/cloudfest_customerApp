b206b72386ca7b7e69d57b8e077b692f5356f4b0
mdghazanfar <ghazanfaralam642786@gmail.com>
2022-04-06 23:45:44 +0530

    created Booking Details page

--- DataStore/DataService.tsx
+++ DataStore/DataService.tsx
@@ -1,5 +1,5 @@
-import electrician from "../assets/images/electrician.png";
-import plumber from "../assets/images/plumber.png";
+import Electrician from "../assets/images/electrician.png";
+import Plumber from "../assets/images/plumber.png";
 import gardener from "../assets/images/gardener.png";
 import barber from "../assets/images/barber.png";
 import cleaner from "../assets/images/cleaner.png";
@@ -9,43 +9,109 @@ import photography from "../assets/images/photography.png";
 
 const DataServices = [
   {
-    img: electrician,
-    name: 'Electrician',
+    id: "1",
+    img: Electrician,
+    name: 'Shams',
+    jobName: "Electrician",
+    exp: "2 years of Exprience",
+    rating: 4.3,
+    totalJobs: 35,
+    rate: 250,
   },
   {
-    img: plumber,
-    name: 'Plumber',
+    id: "2",
+    img: Plumber,
+    name: 'Ghazwanfar',
+    exp: "2 years of Exprience",
+    rating: 4.3,
+    totalJobs: 35,
+    rate: 250,
+    jobName: 'Electrician',
+
   },
   {
+    id: "3",
     img: gardener,
-    name: 'Gardener',
+    name: 'osama',
+    exp: "2 years of Exprience",
+    rating: 4.3,
+    totalJobs: 35,
+    rate: 250,
+    jobName: 'Electrician',
+
   },
   {
+    id: "4",
     img: barber,
-    name: 'Barber',
+    name: 'Huzefa',
+    exp: "2 years of Exprience",
+    rating: 4.3,
+    totalJobs: 35,
+    rate: 250
   },
   {
+    id: "5",
     img: cleaner,
-    name: 'Cleaner',
+    name: 'pritom',
+    exp: "2 years of Exprience",
+    rating: 4.3,
+    totalJobs: 35,
+    rate: 250,
+    jobName: 'Cleaner',
+
   },
   {
+    id: "6",
     img: photography,
-    name: 'Photographer',
+    name: 'Shahid',
+    exp: "2 years of Exprience",
+    rating: 4.3,
+    totalJobs: 35,
+    rate: 250,
+    jobName: 'Photography',
+
   },
   {
-    img: electrician,
-    name: 'Plumber',
+    id: "7",
+    img: Electrician,
+    name: 'Ehtasham',
+    exp: "2 years of Exprience",
+    rating: 4.3,
+    totalJobs: 35,
+    rate: 250,
+    jobName: 'Electrician',
+
   },
   {
+    id: "8",
     img: barber,
-    name: 'Plumber',
+    name: 'Mahaboob',
+    exp: "2 years of Exprience",
+    rating: 4.3,
+    totalJobs: 35,
+    rate: 250,
+    jobName: "Barber",
   },
   {
-    img: electrician,
-    name: 'Plumber',
-  }, {
+    id: "9",
+    img: Electrician,
+    name: 'Sucheta',
+    exp: "2 years of Exprience",
+    rating: 4.3,
+    totalJobs: 35,
+    rate: 250,
+    jobName: 'Electrician',
+
+  },
+  {
+    id: "10",
     img: gardener,
-    name: 'Plumber',
+    name: 'Janvi',
+    exp: "2 years of Exprience",
+    rating: 4.3,
+    totalJobs: 35,
+    rate: 250,
+    jobName: 'Gardener',
   },
 ];
 
