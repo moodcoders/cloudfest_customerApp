d05ac0a7b94eaf64eef013d7a80583a5283b1cc9
suchetamahata <nosuchetamahatano@gmail.com>
2022-04-15 00:22:58 +0530

    resolved errors

--- components/BookingHandymanDetails.tsx
+++ components/BookingHandymanDetails.tsx
@@ -8,7 +8,6 @@ import { useEffect, useState } from 'react';
 import { bookingDataType } from '../screens/MyBooking';
 
 interface bookingParams {
-<<<<<<< HEAD
     bookingData?: bookingDataType
 };
 
@@ -89,92 +88,6 @@ const BookingHandymanDetails = ({ bookingData }: bookingParams) => {
             </View>
         );
     }
-=======
-  img: any;
-  name: string;
-  totalJobs: number;
-  exp: string;
-  rating: number;
-  rate: number;
-  jobName?: any;
-}
-
-const BookingHandymanDetails = (prop: bookingParams) => {
-  return (
-    <View style={styles.card}>
-      <View style={{ flexDirection: 'row' }}>
-        <View style={styles.cardBoarder}>
-          <Image style={styles.image} resizeMode="cover" source={prop.img} />
-          <Text>{prop.jobName}</Text>
-        </View>
-        <View>
-          <Text style={styles.bookingNumber}>Booking Number- bg6789p0 </Text>
-          <Text style={{ marginTop: 25 }}>Smart PRO</Text>
-          <Text style={styles.name}>Shams</Text>
-          <Text style={{ color: 'red' }}>{prop.exp}</Text>
-          <Text style={styles.data}>
-            <Icon
-              name="star"
-              onPress={() => console.log('back button press')}
-              color="gold"
-              style={{ fontSize: 25 }}
-            />
-            {prop.rating}
-          </Text>
-        </View>
-      </View>
-      <View style={{ marginLeft: 10 }}>
-        <Text style={styles.bookingNumber}>Date &#38; time</Text>
-        <Text>
-          <Calender
-            name="calendar-clock"
-            onPress={() => console.log('back button press')}
-            color="#333"
-            style={{ fontSize: 25, margin: 10 }}
-          />{' '}
-          9:20 PM Sat,12 Sep 2021
-        </Text>
-      </View>
-      <View>
-        <Text style={[styles.priceDetails]}>Charge Details </Text>
-        <View
-          style={styles.separator}
-          lightColor="#eee"
-          darkColor="rgba(0, 0, 0, 0.22)"
-        />
-        <View style={styles.service}>
-          <Text style={styles.serviceCharge}>Service Charge</Text>
-          <Text style={styles.serviceCharge}>
-            <Rupee
-              name="rupee"
-              onPress={() => console.log('back button press')}
-              color="#333"
-              style={{ fontSize: 20, fontWeight: 'bold' }}
-            />
-            {prop.rate}
-          </Text>
-        </View>
-        <View
-          style={styles.separator}
-          lightColor="#eee"
-          darkColor="rgba(0, 0, 0, 0.22)"
-        />
-        <View style={styles.service}>
-          <Text style={styles.serviceCharge}>Amount Paid</Text>
-          <Text style={styles.serviceCharge}>
-            <Rupee
-              name="rupee"
-              onPress={() => console.log('back button press')}
-              color="#333"
-              style={{ fontSize: 20, fontWeight: 'bold' }}
-            />
-            {prop.rate}
-          </Text>
-        </View>
-      </View>
-    </View>
-  );
->>>>>>> d0f39a12c2cec292ca0f97f344d5a97dd9f31da5
 };
 
 const styles = StyleSheet.create({
